54b9aab8befc5a0ebf1c2018aa0f45e6784bbeed
VGT <tomek@vgtworld.pl>
2014-10-12 11:51:10 +0200

    Add save file as action.

--- restificator/src/main/java/pl/vgtworld/restificator/gui/actions/SaveFileAction.java
+++ restificator/src/main/java/pl/vgtworld/restificator/gui/actions/SaveFileAction.java
@@ -5,21 +5,17 @@ import pl.vgtworld.restificator.gui.MainWindow;
 import pl.vgtworld.restificator.gui.tabs.TabbedPane;
 import pl.vgtworld.restificator.io.SaveException;
 import pl.vgtworld.restificator.io.ScriptSerializer;
-import pl.vgtworld.restificator.utils.RestificatorFileFilter;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 import javax.swing.AbstractAction;
-import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import java.awt.event.ActionEvent;
 
 @Singleton
 public class SaveFileAction extends AbstractAction {
-
-	private JFileChooser saveDialog;
 
 	@Inject
 	private TabbedPane pane;
@@ -27,22 +23,19 @@ public class SaveFileAction extends AbstractAction {
 	@Inject
 	private Provider<MainWindow> mainWindowProvider;
 
+	@Inject
+	private SaveFileAsAction saveAsAction;
+
 	@PostConstruct
 	private void init() {
 		putValue(NAME, "Save");
-		saveDialog = new JFileChooser();
-		saveDialog.setFileFilter(new RestificatorFileFilter());
-		saveDialog.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		saveDialog.setDialogType(JFileChooser.SAVE_DIALOG);
-		saveDialog.setMultiSelectionEnabled(false);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		String editedFilepath = mainWindowProvider.get().getEditedFile();
 		if (editedFilepath == null) {
-			//TODO Redirect action to SaveFileAsAction.
-			JOptionPane.showMessageDialog(pane, "Unsupported operation", "Saving error", JOptionPane.ERROR_MESSAGE);
+			saveAsAction.actionPerformed(event);
 			return;
 		}
 		RestificatorExecutionData data = pane.readData();
--- restificator/src/main/java/pl/vgtworld/restificator/gui/actions/SaveFileAsAction.java
+++ restificator/src/main/java/pl/vgtworld/restificator/gui/actions/SaveFileAsAction.java
@@ -1,7 +1,54 @@
 package pl.vgtworld.restificator.gui.actions;
 
-/**
- * Created by vgt on 10/12/14.
- */
-public class SaveFileAsAction {
+import pl.vgtworld.restificator.data.RestificatorExecutionData;
+import pl.vgtworld.restificator.gui.MainWindow;
+import pl.vgtworld.restificator.gui.tabs.TabbedPane;
+import pl.vgtworld.restificator.io.SaveException;
+import pl.vgtworld.restificator.io.ScriptSerializer;
+import pl.vgtworld.restificator.utils.RestificatorFileFilter;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.swing.AbstractAction;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+public class SaveFileAsAction extends AbstractAction {
+
+	private JFileChooser saveDialog;
+
+	@Inject
+	private TabbedPane pane;
+
+	@Inject
+	private Provider<MainWindow> mainWindowProvider;
+
+	@PostConstruct
+	private void init() {
+		putValue(NAME, "Save As");
+		saveDialog = new JFileChooser();
+		saveDialog.setFileFilter(new RestificatorFileFilter());
+		saveDialog.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		saveDialog.setDialogType(JFileChooser.SAVE_DIALOG);
+		saveDialog.setMultiSelectionEnabled(false);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		int saveDialogStatus = saveDialog.showSaveDialog(pane);
+		if (saveDialogStatus == JFileChooser.APPROVE_OPTION) {
+			try {
+				File selectedFile = saveDialog.getSelectedFile();
+				RestificatorExecutionData data = pane.readData();
+				ScriptSerializer serializer = new ScriptSerializer();
+				serializer.save(data, selectedFile);
+				mainWindowProvider.get().setEditedFile(selectedFile.getAbsolutePath());
+			} catch (SaveException e) {
+				JOptionPane.showMessageDialog(pane, e.getMessage(), "Saving error", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
 }
