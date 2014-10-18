2571294cdc68368daa7f65cc985f6094b45f42cb
VGT <tomek@vgtworld.pl>
2014-10-18 11:51:17 +0200

    Add save and cancel buttons to request form dialog.

--- restificator/src/main/java/pl/vgtworld/restificator/gui/tabs/requests/form/RequestsFormDialog.java
+++ restificator/src/main/java/pl/vgtworld/restificator/gui/tabs/requests/form/RequestsFormDialog.java
@@ -1,14 +1,23 @@
 package pl.vgtworld.restificator.gui.tabs.requests.form;
 
+import pl.vgtworld.utils.awt.Fill;
+import pl.vgtworld.utils.awt.GridBagConstraintsImproved;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-import java.awt.GridLayout;
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
 
 public class RequestsFormDialog extends JDialog {
 
 	private LeftFormPanel leftPanel;
 
 	private RightFormPanel rightPanel;
+
+	private boolean saved;
 
 	public RequestsFormDialog(JFrame owner) {
 		super(owner);
@@ -18,11 +27,42 @@ public class RequestsFormDialog extends JDialog {
 		setSize(800,600);
 	}
 
-	private void init() {
-		setLayout(new GridLayout(1, 2));
+	public boolean isSaved() {
+		return saved;
+	}
 
-		add(leftPanel);
-		add(rightPanel);
+	private void init() {
+		GridBagConstraintsImproved gbc = new GridBagConstraintsImproved();
+		JButton saveButton = new JButton("Save");
+		JButton cancelButton = new JButton("Cancel");
+		JPanel buttonPanel = new JPanel();
+
+		setLayout(new GridBagLayout());
+
+		buttonPanel.add(saveButton);
+		buttonPanel.add(cancelButton);
+
+		gbc.setGrid(0, 0).setGridSize(1, 1).setWeight(100, 100).setInsets(0).setFill(Fill.BOTH);
+		add(leftPanel, gbc);
+		gbc.setGrid(1, 0).setGridSize(1, 1).setWeight(100, 100).setInsets(0).setFill(Fill.BOTH);
+		add(rightPanel, gbc);
+		gbc.setGrid(0, 1).setGridSize(2, 1).setWeight(100, 0).setInsets(5).setFill(Fill.BOTH);
+		add(buttonPanel, gbc);
+
+		cancelButton.addActionListener(new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saved = false;
+				setVisible(false);
+			}
+		});
+		saveButton.addActionListener(new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saved = true;
+				setVisible(false);
+			}
+		});
 	}
 
 }
