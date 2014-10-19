5e2690b4346d3afab60e5e719180f51e510405ea
VGT <tomek@vgtworld.pl>
2014-10-19 14:41:20 +0200

    Implement creating new header in request form dialog.

--- restificator/src/main/java/pl/vgtworld/restificator/gui/tabs/requests/form/headers/HeadersPanel.java
+++ restificator/src/main/java/pl/vgtworld/restificator/gui/tabs/requests/form/headers/HeadersPanel.java
@@ -1,8 +1,10 @@
 package pl.vgtworld.restificator.gui.tabs.requests.form.headers;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.SwingUtilities;
 import java.awt.BorderLayout;
 
 public class HeadersPanel extends JPanel {
@@ -20,7 +22,8 @@ public class HeadersPanel extends JPanel {
 	private void init() {
 		tableModel = new TableModel();
 		table = new JTable(tableModel);
-		NewHeaderAction newHeaderAction = new NewHeaderAction();
+		JFrame window = (JFrame) SwingUtilities.getWindowAncestor(this);
+		NewHeaderAction newHeaderAction = new NewHeaderAction(window, tableModel);
 		EditHeaderAction editHeaderAction = new EditHeaderAction();
 		DeleteHeadersAction deleteHeadersAction = new DeleteHeadersAction();
 		buttonBar = new ButtonBar(newHeaderAction, editHeaderAction, deleteHeadersAction);
