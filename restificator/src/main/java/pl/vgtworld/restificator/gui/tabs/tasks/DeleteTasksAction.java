8fa4e689c79602dd73a0df9ad68a5046e1adf6f6
VGT <tomek@vgtworld.pl>
2014-10-23 21:38:32 +0200

    Create button panel for tasks tab.

---
+++ restificator/src/main/java/pl/vgtworld/restificator/gui/tabs/tasks/DeleteTasksAction.java
@@ -0,0 +1,17 @@
+package pl.vgtworld.restificator.gui.tabs.tasks;
+
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+class DeleteTasksAction extends AbstractAction {
+
+	DeleteTasksAction() {
+		putValue(NAME, "Delete");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//TODO Implementation.
+	}
+
+}
---
+++ restificator/src/main/java/pl/vgtworld/restificator/gui/tabs/tasks/NewTaskAction.java
@@ -0,0 +1,17 @@
+package pl.vgtworld.restificator.gui.tabs.tasks;
+
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+class NewTaskAction extends AbstractAction {
+
+	NewTaskAction() {
+		putValue(NAME, "New");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//TODO Implementation.
+	}
+
+}
