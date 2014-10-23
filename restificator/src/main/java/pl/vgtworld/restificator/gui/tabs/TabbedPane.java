b9bf80cfb2aef1144d111a48d0d65b9d09c1db7d
VGT <tomek@vgtworld.pl>
2014-10-23 21:39:13 +0200

    Create tasks panel and insert it into TabbedPane.

--- restificator/src/main/java/pl/vgtworld/restificator/gui/tabs/TabbedPane.java
+++ restificator/src/main/java/pl/vgtworld/restificator/gui/tabs/TabbedPane.java
@@ -4,6 +4,7 @@ import pl.vgtworld.restificator.data.RestificatorExecutionData;
 import pl.vgtworld.restificator.gui.tabs.globalheaders.GlobalHeadersPanel;
 import pl.vgtworld.restificator.gui.tabs.requests.RequestsPanel;
 import pl.vgtworld.restificator.gui.tabs.settings.SettingsPanel;
+import pl.vgtworld.restificator.gui.tabs.tasks.TasksPanel;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -20,6 +21,9 @@ public class TabbedPane extends JTabbedPane {
 	private GlobalHeadersPanel globalHeaders;
 
 	@Inject
+	private TasksPanel tasksPanel;
+
+	@Inject
 	private RequestsPanel requestsPanel;
 
 	@PostConstruct
@@ -27,19 +31,21 @@ public class TabbedPane extends JTabbedPane {
 		addTab("Settings", settings);
 		addTab("Global headers", globalHeaders);
 		addTab("Parameters", null);
-		addTab("Tasks", null);
+		addTab("Tasks", tasksPanel);
 		addTab("Requests", requestsPanel);
 	}
 
 	public void cleanData() {
 		settings.cleanData();
 		globalHeaders.cleanData();
+		tasksPanel.cleanData();
 		requestsPanel.cleanData();
 	}
 
 	public void fillWithData(RestificatorExecutionData data) {
 		settings.fillWithData(data.getSettings());
 		globalHeaders.fillWithData(data.getGlobalHeaders());
+		tasksPanel.fillWithData(data.getTasks());
 		requestsPanel.fillWithData(data.getRequests());
 	}
 
@@ -47,6 +53,7 @@ public class TabbedPane extends JTabbedPane {
 		RestificatorExecutionData data = new RestificatorExecutionData();
 		data.setSettings(settings.readData());
 		data.setGlobalHeaders(globalHeaders.readData());
+		data.setTasks(tasksPanel.readData());
 		data.setRequests(requestsPanel.readData());
 		return data;
 	}
---
+++ restificator/src/main/java/pl/vgtworld/restificator/gui/tabs/tasks/TasksPanel.java
@@ -0,0 +1,49 @@
+package pl.vgtworld.restificator.gui.tabs.tasks;
+
+import pl.vgtworld.restificator.data.executionqueue.Task;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.List;
+
+@Singleton
+public class TasksPanel extends JPanel {
+
+	private TableModel tableModel;
+
+	private JTable table;
+
+	private ButtonBar buttonBar;
+
+	public void cleanData() {
+		//TODO Implementation.
+	}
+
+	public void fillWithData(List<Task> tasks) {
+		//TODO Implementation
+	}
+
+	public List<Task> readData() {
+		//TODO Implementation.
+		return new ArrayList<>();
+	}
+
+	@PostConstruct
+	private void init() {
+		tableModel = new TableModel();
+		table = new JTable(tableModel);
+		NewTaskAction newTaskAction = new NewTaskAction();
+		EditTaskAction editTaskAction = new EditTaskAction();
+		DeleteTasksAction deleteTasksAction = new DeleteTasksAction();
+		buttonBar = new ButtonBar(newTaskAction, editTaskAction, deleteTasksAction);
+
+		setLayout(new BorderLayout());
+		add(buttonBar, BorderLayout.PAGE_START);
+		add(new JScrollPane(table), BorderLayout.CENTER);
+	}
+}
